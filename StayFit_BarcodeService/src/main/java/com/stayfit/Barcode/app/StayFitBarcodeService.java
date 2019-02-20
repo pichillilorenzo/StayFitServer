3d1653bf8cf186994e462df8f23a261644c12688
gibsmat <matteoricci229@gmail.com>
2019-02-20 17:24:57 +0100

    Add Barcode Service
    Add Barcode Service

---
+++ StayFit_BarcodeService/src/main/java/com/stayfit/Barcode/app/StayFitBarcodeService.java
@@ -0,0 +1,14 @@
+package com.stayfit.Barcode.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayFitBarcodeService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayFitBarcodeService.class, args);
+	}
+
+}
+
