83ccdafe61e4e399be79dff903ae2907ebc1e77d
pichillilorenzo <pichillilorenzo@gmail.com>
2019-03-01 12:44:08 +0100

    updated README.md

--- Load_Balancer/index.js
+++ Load_Balancer/index.js
@@ -1,15 +1,24 @@
-const http = require('http');
-const request = require('request');
+const http = require("http");
+const request = require("request");
 const path = require("path");
 
 const ABS_PATH = path.dirname(__dirname);
 
 // available hosts
 let serviceHosts = {
-  "/user/services/user": [{pm2: "http://localhost:8071", java: "http://localhost:8082"}, {pm2: "http://localhost:8072", java: "http://localhost:8092"}],
-  "/user_history/services/user_history": [{pm2: "http://localhost:8073", java: "http://localhost:8083"}, {pm2: "http://localhost:8074", java: "http://localhost:8093"}],
-  "/user_diet/services/user_diet": [{pm2: "http://localhost:8075", java: "http://localhost:8084"}, {pm2: "http://localhost:8076", java: "http://localhost:8094"}]
-}
+  "/user/services/user": [
+    { pm2: "http://localhost:8071", java: "http://localhost:8082" },
+    { pm2: "http://localhost:8072", java: "http://localhost:8092" }
+  ],
+  "/user_history/services/user_history": [
+    { pm2: "http://localhost:8073", java: "http://localhost:8083" },
+    { pm2: "http://localhost:8074", java: "http://localhost:8093" }
+  ],
+  "/user_diet/services/user_diet": [
+    { pm2: "http://localhost:8075", java: "http://localhost:8084" },
+    { pm2: "http://localhost:8076", java: "http://localhost:8094" }
+  ]
+};
 
 let cachedResourceInfo = {
   "/user/services/user": [],
@@ -21,10 +30,9 @@ let cachedResourceInfo = {
 setTimeout(() => {
   let intervalId = setInterval(async () => {
     for (let key of Object.keys(serviceHosts)) {
-      for(let i = 0; i < serviceHosts[key].length; i++) {
+      for (let i = 0; i < serviceHosts[key].length; i++) {
         request(serviceHosts[key][i].pm2, (error, response, body) => {
-          if (error) 
-            console.log(error);
+          if (error) console.log(error);
           else if (response.statusCode == 200) {
             try {
               cachedResourceInfo[key][i] = JSON.parse(body);
@@ -42,33 +50,39 @@ setTimeout(() => {
 // returns a value based on CPU usage, Memory usage and number of requests in 1 seconds
 // to determine which web service is the best to serve the current request
 function calcWeightedRes(cpu, memory, numOfRequests) {
-  return (cpu * 0.40) + (memory * 0.20) + (numOfRequests * 0.40);
+  return cpu * 0.4 + memory * 0.2 + numOfRequests * 0.4;
 }
 
-// create a server to accept requests from the Prosumer and forward the requests 
+// create a server to accept requests from the Prosumer and forward the requests
 // to the best web service
-http.createServer(function (req, res) {
+http
+  .createServer(function(req, res) {
+    let hosts = serviceHosts[req.url];
+    let resources = cachedResourceInfo[req.url];
+    let weighetRes = [];
 
-  let hosts = serviceHosts[req.url];
-  let resources = cachedResourceInfo[req.url];
-  let weighetRes = [];
+    for (let i = 0; i < resources.length; i++) {
+      weighetRes.push(
+        calcWeightedRes(
+          resources[i].cpu,
+          resources[i].memory,
+          resources[i].numOfRequests
+        )
+      );
+    }
 
-  for(let i = 0; i < resources.length; i++) {
-    weighetRes.push(calcWeightedRes(resources[i].cpu, resources[i].memory, resources[i].numOfRequests));
-  }
-  
-  let index = weighetRes.indexOf(Math.min(...weighetRes));
-  index = (index < 0) ? 0 : index;
-  if (cachedResourceInfo[req.url][index].numOfRequests != null)
-    cachedResourceInfo[req.url][index].numOfRequests++;
+    let index = weighetRes.indexOf(Math.min(...weighetRes));
+    index = index < 0 ? 0 : index;
+    if (cachedResourceInfo[req.url][index].numOfRequests != null)
+      cachedResourceInfo[req.url][index].numOfRequests++;
 
-  // forward request
-  req.pipe(request(hosts[index].java + req.url)).pipe(res);
+    // forward request
+    req.pipe(request(hosts[index].java + req.url)).pipe(res);
+  })
+  .listen(process.env.PORT, function() {
+    console.log("Listening on port " + process.env.PORT);
+  });
 
-}).listen(process.env.PORT, function(){
-  console.log("Listening on port " + process.env.PORT)
-});
-
-process.on('uncaughtException', (e) => {
-  console.log('[uncaughtException]: ', e.stack);
+process.on("uncaughtException", e => {
+  console.log("[uncaughtException]: ", e.stack);
 });
--- README.md
+++ README.md
@@ -1,1 +1,108 @@
-# StayFitServer
+# StayFit Servers
+
+## Dependencies
+
+- java
+- node.js + npm (https://nodejs.org/en/)
+- mvn (https://maven.apache.org/)
+- pm2 (https://www.npmjs.com/package/pm2): `npm install pm2 -g`
+
+## Installation
+
+### Node.js dependencies installation
+
+To install Node.js dependencies for `Load_Balancer` and `Server_Amazon`, go in each of these directories and execute `npm install`.
+
+### Database MySQL
+
+To initialize the Database MySQL, you can import the `./sql/stayfit.sql` file. 
+This file contains all the MySQL tables needed to execute the application properly.
+
+Default values are:
+- Database name: `stayfit`
+- Database username: `stayfit`
+- Database password: `stayfit`
+
+### Database MongoDB
+
+Create a database named: `stayfit`.
+
+## Getting Started
+
+To build .jar files and execute the Java and Node.js servers together, you can use `./start.sh -b`.
+Instead, if you don't want to build, you can simply use `./start.sh`.
+
+The `./start.sh` script contains the default host value of each server:
+```bash
+...
+
+STAYFIT_SERVER_HOST="localhost:8080"
+OAUTH2_SERVICE_HOST="localhost:8081"
+LOAD_BALANCER_HOST="localhost:8070"
+
+USER_SERVICE_HOST_1="localhost:8082"
+USER_HISTORY_SERVICE_HOST_1="localhost:8083"
+USER_DIET_SERVICE_HOST_1="localhost:8084"
+
+USER_SERVICE_HOST_2="localhost:8092"
+USER_HISTORY_SERVICE_HOST_2="localhost:8093"
+USER_DIET_SERVICE_HOST_2="localhost:8094"
+
+MYSQL_DATABASE_URL="jdbc:mysql://localhost:3306/stayfit?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"
+MONGODB_DATABASE_URL="localhost:27017"
+
+...
+```
+
+Also, the `./start.sh` script contains the default PORT value of each **Monitor** and for the `Server_Amazon` server:
+```bash
+# USER SERVICE 1
+export PORT=8071
+# USER SERVICE 2
+export PORT=8072
+
+# USER HISTORY SERVICE 1
+export PORT=8073
+# USER HISTORY SERVICE 2
+export PORT=8074
+
+# USER DIET SERVICE 1
+export PORT=8075
+# USER DIET SERVICE 2
+export PORT=8076
+
+# OAUTH2 SERVICE
+export PORT=8077
+
+# AMAZON API
+export PORT=3000
+
+# LOAD BALANCER
+export PORT=8070
+
+# PROSUMER
+export PORT=8078
+```
+
+The `./Load_Balancer/index.js` script contains also the host value of each **Monitor** and **Java Server**, that are:
+```javascript
+...
+
+// available hosts
+let serviceHosts = {
+  "/user/services/user": [
+    { pm2: "http://localhost:8071", java: "http://localhost:8082" },
+    { pm2: "http://localhost:8072", java: "http://localhost:8092" }
+  ],
+  "/user_history/services/user_history": [
+    { pm2: "http://localhost:8073", java: "http://localhost:8083" },
+    { pm2: "http://localhost:8074", java: "http://localhost:8093" }
+  ],
+  "/user_diet/services/user_diet": [
+    { pm2: "http://localhost:8075", java: "http://localhost:8084" },
+    { pm2: "http://localhost:8076", java: "http://localhost:8094" }
+  ]
+};
+
+...
+```
